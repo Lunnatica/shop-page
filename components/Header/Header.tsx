3ebb41a0b52a7555d345b392db8db3ea1c2b0fd4
Lunnatica <paulalopezantelo@gmail.com>
2021-02-06 18:42:03 +0000

    Add Header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { StyledHeader } from './StyledHeader';
+
+const Header = () => (
+    <StyledHeader>
+        <h1>The test shop</h1>
+    </StyledHeader>
+);
+
+export { Header };
---
+++ components/Header/StyledHeader.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+`;
+
+export { StyledHeader };
